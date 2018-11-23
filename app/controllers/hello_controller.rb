1e20929983d556e4559bef4a70a3a52edd443796
telechan <sou.s0804@gmail.com>
2018-11-23 16:30:19 +0900

    add routes.rb again

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,7 +2,12 @@ class HelloController < ApplicationController
   def index
     render plain: 'こんにちは、世界！'
   end
+<<<<<<< HEAD
   def view
     @msg = 'こんにちは、世界！'
+=======
+  def list
+    @books = Book.all
+>>>>>>> add routes.rb again
   end
 end
