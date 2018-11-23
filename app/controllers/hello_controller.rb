546553f4fdeac30d9c05fe23f74cbc9792071b5c
telechan <sou.s0804@gmail.com>
2018-11-23 16:34:20 +0900

    change hello_controller again

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -3,9 +3,15 @@ class HelloController < ApplicationController
     render plain: 'こんにちは、世界！'
   end
 <<<<<<< HEAD
+<<<<<<< HEAD
   def view
     @msg = 'こんにちは、世界！'
 =======
+=======
+  def view
+    @msg = 'こんにちは、世界！'
+  end
+>>>>>>> change hello_controller again
   def list
     @books = Book.all
 >>>>>>> add routes.rb again
